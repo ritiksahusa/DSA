c70ec812cb6cbb37ce06f62c218cbfbad987f84a
ritik.sahu.sa@gmail.com <ritik.sahu.sa@gmail.com>
2021-05-30 09:10:43 +0530

    Ladder Top Down

--- DynamicProgramming/LadderProblem/LadderProblem.java
+++ DynamicProgramming/LadderProblem/LadderProblem.java
@@ -1,12 +1,40 @@
 package LadderProblem;
 
+import java.util.Arrays;
 /*
     Given a Ladder of Size n and in integer k find the number of ways to climb the ladder if atmost k steps are allowed at a time
 */
 
 class LadderSoln {
 
+    int arr[];
+
+    public LadderSoln() {
+        arr = new int[1000];// sample size
+        Arrays.fill(arr, -1);
+        arr[0] = 1;
+    }
+
+    // complexity analyzer(check the count in debugger)
+    int count1 = 0;
+    int count2 = 0;
+
+    public int topDown(int n, int k) {
+        count1++;
+        if (n < 0)
+            return 0;
+        if (arr[n] != -1)
+            return arr[n];
+        int count = 0;
+        for (int i = 1; i <= k; i++) {
+            count = count + topDown(n - i, k);
+        }
+
+        return arr[n] = count;
+    }
+
     public int recursive(int n, int k) {
+        count2++;
         if (n == 0) // base case
             return 1;
         if (n < 0)
@@ -24,7 +52,9 @@ public class LadderProblem {
 
     public static void main(String[] args) {
         LadderSoln soln = new LadderSoln();
-        System.out.println(soln.recursive(4, 3));
+        System.out.println(soln.topDown(20, 3));// sample values
+        System.out.println(soln.recursive(20, 3));
+        System.out.println();
 
     }
 }
